4673e9adf3fda7ed587673abb69805b3b7ab5c5a
ggionet1 <gagi0247@colorado.edu>
2023-09-13 13:53:51 +0000

    added a csv file Agrofood_co2_emission and updated function get_column in my_utils.py to extract data from a column conditionally

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/oQi7O4AA)
 # python-refresher
+
+Updated my_utils.py to define a function to return an array with desired values from a given column, given that the row has satisfied a specified condition. I then uploaded a csv (Agrofood_co2_emissions.csv), which will later be important for the project.
--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,18 @@
+#The purpose of this code is to generate an array containing information from the result column when a query column matches a specific value
+
+#I did not specify what the query_value datatype should be so that this code worked for a wider variety of columns
+
+#Columns should be specified by index (order within the file) rather than name
+
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    fire_array = []
+    f = open(file_name, 'r')
+    for l in f:
+        row = l.rstrip().split(',')
+        selected_column = row[query_column]
+        result = row[result_column]
+        if selected_column == query_value:
+            fire_array.append(result)
+
+    f.close()
+    print(fire_array)
