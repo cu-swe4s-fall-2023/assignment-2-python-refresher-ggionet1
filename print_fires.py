6733ab9fd7da5e79cc5bc60a07c9dd6926bca347
ggionet1 <gagi0247@colorado.edu>
2023-09-14 03:09:06 +0000

    Updated print_fires.py to use the my_utils get_column function to print number of fires in the USA

--- print_fires.py
+++ print_fires.py
@@ -1,3 +1,5 @@
 #Using Area, Extract number of Forest Fires for United States of America
 
+from my_utils import get_column
+
 get_column('Agrofood_co2_emission.csv', 0, "United States of America", 3)
